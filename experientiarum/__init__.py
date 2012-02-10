4c4f50956f21de3425a2587e28ed695541d0306d
Mark Feltner <feltner.mj@gmail.com>
2012-02-09 20:50:54 -0600

    changed logging

--- experientiarum/__init__.py
+++ experientiarum/__init__.py
@@ -184,9 +184,9 @@ def generate_app(config):
     app = Flask(__name__, static_folder = 'static', template_folder = 'templates')
     app.config.from_object(config)
     
+    configure_logging(app)
     configure_blueprints(app)
     configure_extensions(app)
-    configure_logging(app)
     configure_identity(app)
     configure_template_filters(app)
     
