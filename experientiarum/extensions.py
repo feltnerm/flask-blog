90640493924d7c1976489741b76e2aeddfbdef00
Mark Feltner <feltner.mj@gmail.com>
2012-02-14 22:58:05 -0600

    removed logbook dependency

--- experientiarum/extensions.py
+++ experientiarum/extensions.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
-from logbook import NestedSetup, NullHandler, RotatingFileHandler, Processor 
 from flaskext.mongokit import MongoKit
 
 db = MongoKit()
-
-log = NestedSetup([
-            NullHandler(),
-            RotatingFileHandler('log/warning.log', level='WARNING'),
-            RotatingFileHandler('log/error.log', level='ERROR'),
-            RotatingFileHandler('log/debug.log', level='DEBUG'),
-    ])
